2d03b7f0bb90654de1fb7ce62103ecf8521f01b6
CesValde <valderramacesar2002@gmail.com>
2026-04-08 00:37:11 -0300

    add: controller
    adoption

---
+++ src/controllers/adoptions.controller.js
@@ -0,0 +1,94 @@
+import adoptionsService from "../Service/adoptions.service.js"
+import adoptionsDTO from "../dtos/adoptions.dto.js"
+
+export const getAllAdoptions = async (req, res) => {
+   try {
+      const adoptionss = await adoptionsService.getAll()
+      return res.status(200).json(adoptionss.map(adoptionsDTO.fromDB))
+   } catch (error) {
+      return res.status(error.statusCode || 500).json({
+         error: error.statusCode ? error.message : "Internal server error"
+      })
+   }
+}
+
+export const getAdoptionByIdByPopulated = async (req, res) => {
+   const { uid } = req.params
+
+   try {
+      const adoptions = await adoptionsService.getByIdByPopulated(uid)
+      return res.status(200).json(adoptionsDTO.fromDB(adoptions))
+   } catch (error) {
+      return res.status(error.statusCode || 500).json({
+         error: error.statusCode ? error.message : "Internal server error"
+      })
+   }
+}
+
+export const getAdoptionById = async (req, res) => {
+   const { uid } = req.params
+
+   try {
+      const adoptions = await adoptionsService.getById(uid)
+      return res.status(200).json(adoptionsDTO.fromDB(adoptions))
+   } catch (error) {
+      return res.status(error.statusCode || 500).json({
+         error: error.statusCode ? error.message : "Internal server error"
+      })
+   }
+}
+
+export const saveAdoption = async (req, res) => {
+   const data = req.body
+
+   try {
+      const adoptions = await adoptionsService.create(data)
+      const adoptionsCreate = adoptionsDTO.fromDB(adoptions)
+
+      return res.status(201).json({
+         message: "Adoption created successfully",
+         adoptionsCreate
+      })
+   } catch (error) {
+      return res.status(error.statusCode || 500).json({
+         error: error.statusCode ? error.message : "Internal server error"
+      })
+   }
+}
+
+export const updateAdoption = async (req, res) => {
+   const { uid } = req.params
+   const data = req.body
+
+   try {
+      const adoptions = await adoptionsService.update(uid, data)
+      const adoptionsUpdate = adoptionsDTO.fromDB(adoptions)
+
+      return res.status(200).json({
+         message: "Adoption update successfully",
+         adoptionsUpdate
+      })
+   } catch (error) {
+      return res.status(error.statusCode || 500).json({
+         error: error.statusCode ? error.message : "Internal server error"
+      })
+   }
+}
+
+export const deleteAdoption = async (req, res) => {
+   const { uid } = req.params
+
+   try {
+      const adoptions = await adoptionsService.delete(uid)
+      const adoptionsDelete = adoptionsDTO.fromDB(adoptions)
+
+      return res.status(200).json({
+         message: "Adoption delete successfully",
+         adoptionsDelete
+      })
+   } catch (error) {
+      return res.status(error.statusCode || 500).json({
+         error: error.statusCode ? error.message : "Internal server error"
+      })
+   }
+}
