22a9e380bd070cc3050754ff0461a5c69d6dbdd4
CesValde <valderramacesar2002@gmail.com>
2026-04-10 18:12:54 -0300

    change: dto
    adoption

--- src/DTO/adoption.dto.js
+++ src/DTO/adoption.dto.js
@@ -5,23 +5,26 @@ class AdoptionDTO {
       return {
          id: adoption._id,
 
-         user: adoption.user
-            ? {
-                 id: adoption.user._id,
-                 name: adoption.user.first_name + " " + adoption.user.last_name,
-                 email: adoption.user.email,
-                 pets: adoption.user.pets
-              }
-            : adoption.user,
+         user:
+            adoption.user == {}
+               ? {
+                    id: adoption.user._id,
+                    name:
+                       adoption.user.first_name + " " + adoption.user.last_name,
+                    email: adoption.user.email,
+                    pets: adoption.user.pets
+                 }
+               : adoption.user,
 
-         pet: adoption.pet
-            ? {
-                 id: adoption.pet._id,
-                 name: adoption.pet.name,
-                 type: adoption.pet.type,
-                 age: adoption.pet.age
-              }
-            : adoption.pet,
+         pet:
+            adoption.pet == {}
+               ? {
+                    id: adoption.pet._id,
+                    name: adoption.pet.name,
+                    type: adoption.pet.type,
+                    age: adoption.pet.age
+                 }
+               : adoption.pet,
 
          status: adoption.status,
          createdAt: adoption.createdAt
